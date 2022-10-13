569d7a96aed87cf9831be0a4bb4f66fcb34cc2a3
T-Hash06 <tomasfelipe.p@gmail.com>
2022-10-13 17:07:10 -0500

    [CREATE] Helmet package

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap(): Promise<void> {
 	const app = await NestFactory.create(AppModule);
 
 	app.useGlobalPipes(new ValidationPipe());
+	app.use(helmet());
 	await app.listen(3000);
 }
 bootstrap();
